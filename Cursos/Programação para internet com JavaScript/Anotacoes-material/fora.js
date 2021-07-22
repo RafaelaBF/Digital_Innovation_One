492962dff835bd13b1f2aa4d2327082a764b84c3
Rafaela Bezerra de Fuigueiredo <82965842+RafaelaBF@users.noreply.github.com>
2021-07-22 20:53:12 -0300

    curso

--- Cursos/Programação para internet com JavaScript/Anotacoes-material/fora.js
+++
@@ -1,5 +0,0 @@
-var nomevariavel = "uma string" // estou declarando uma variável do tipo string
-
-alert ("Algo que deva ser alterado") // Serve para alertar algo antes da página carregar
-
-alert ("Algo que deva ser alterado") // Concatenar uma string
